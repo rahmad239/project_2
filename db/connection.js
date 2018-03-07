e60ff37576aacfb00f4ad769cc6c118d9b02fe0d
Qya Ahmad <raqayyah@msn.com>
2018-03-07 13:08:31 -0500

    trying heroku

--- db/connection.js
+++ db/connection.js
@@ -4,12 +4,11 @@
 // allows us to export it so we can call this in another file
 // it allows us to connect to our database when we want
 const mongoose = require("mongoose");
-mongoose.Promise = Promise;
 
 if (process.env.NODE_ENV == "production") {
   mongoose.connect(process.env.MLAB_URL);
 } else {
   mongoose.connect("mongodb://localhost/ggb");
 }
-
+mongoose.Promise = Promise;
 module.exports = mongoose;
